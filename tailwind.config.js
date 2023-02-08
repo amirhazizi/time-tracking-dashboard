e1ee6a5f9a276a017bb3f7af243ec986473847e6
amir <amirazizihosein@gmail.com>
2023-02-08 23:15:32 +0330

    small screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
         cl_Blue: "hsl(246, 80%, 60%)",
         cl_Lightred: "hsl(15, 100%, 70%)",
         cl_Softblue: "hsl(195, 74%, 62%)",
-        cl_Lightred: "hsl(348, 100%, 68%)",
+        cl_Lightred_2: "hsl(348, 100%, 68%)",
         cl_Limegreen: "hsl(145, 58%, 55%)",
         cl_Violet: "hsl(264, 64%, 52%)",
         cl_Softorange: "hsl(43, 84%, 65%)",
